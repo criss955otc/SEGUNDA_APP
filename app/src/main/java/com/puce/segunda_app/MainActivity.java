25cf5dd7a2feeb72824a6c8443a3366703cb2930
criss955otc <criss955otc@gmail.com>
2026-06-06 03:36:41 -0500

    Primer commit de la segunda app

---
+++ app/src/main/java/com/puce/segunda_app/MainActivity.java
@@ -0,0 +1,59 @@
+package com.puce.segunda_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MaterialButton btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                (v, insets) -> {
+                    Insets systemBars = insets.getInsets(
+                            WindowInsetsCompat.Type.systemBars());
+
+                    v.setPadding(
+                            systemBars.left,
+                            systemBars.top,
+                            systemBars.right,
+                            systemBars.bottom);
+
+                    return insets;
+                });
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> {
+
+            Toast.makeText(
+                    MainActivity.this,
+                    "Bienvenido a EcoBike 🚲",
+                    Toast.LENGTH_SHORT
+            ).show();
+
+            Intent intent = new Intent(
+                    MainActivity.this,
+                    Login.class
+            );
+
+            startActivity(intent);
+
+        });
+    }
+}
